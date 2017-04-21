a303d426295f3c43a0106a313528abaf496d0b07
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-04-21 09:59:03 +0800

    write low resolution images

--- SplitVideoToImageList/main.cpp
+++ SplitVideoToImageList/main.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include <iostream>
 
 using namespace  cv;
@@ -11,16 +13,36 @@ int main()
 	if(videoSource.isOpened())
 	{
 		Mat currentFrame;
-		auto winname = "Current Frame";
-		namedWindow(winname);
+		Mat lrFrame;
+		Mat grayImage;
+
+		auto currentFrameWinname = "Current Frame";
+		auto lowResolutionFrameWinname = "Low Resolution Frame";
+		namedWindow(currentFrameWinname);
+		namedWindow(lowResolutionFrameWinname);
+
+		auto idx = 0;
+		char framename[10];
+
 		while (true)
 		{
 			videoSource >> currentFrame;
 			if(currentFrame.empty())
 				break;
 
-			imshow(winname, currentFrame);
+			cvtColor(currentFrame, grayImage, CV_BGR2GRAY);
+
+			Mat tempMat;
+			pyrDown(grayImage, tempMat);
+			pyrDown(tempMat, lrFrame);
+
+			imshow(currentFrameWinname, currentFrame);
+			imshow(lowResolutionFrameWinname,lrFrame);
 			waitKey(100);
+
+			sprintf(framename, "%d.png", idx);
+			imwrite(framename, lrFrame);
+			idx++;
 		}
 
 		destroyAllWindows();
