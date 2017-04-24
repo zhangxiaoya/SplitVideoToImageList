1403df325203b65ad804adeabba52c46036497db
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-04-24 11:42:42 +0800

    add frame writer base class

---
+++ SplitVideoToImageList/FrameWriterBase.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include "VideoSplitBase.hpp"
+
+class FrameWriterbase
+{
+public:
+	explicit FrameWriterbase(std::string output_folder_name = "");
+
+	void SetVideoSpliter(VideoSplitBase* video_spliter);
+
+	int OutputFrameStream() const;
+
+private:
+	std::string outputFolderName;
+	VideoSplitBase* videoSpliter;
+};
+
+inline FrameWriterbase::FrameWriterbase(std::string output_folder_name): outputFolderName(output_folder_name), videoSpliter(nullptr)
+{
+}
+
+inline void FrameWriterbase::SetVideoSpliter(VideoSplitBase* video_spliter)
+{
+	this->videoSpliter = video_spliter;
+}
+
+inline int FrameWriterbase::OutputFrameStream() const
+{
+	if (videoSpliter == nullptr)
+		return -1;
+
+	char outputFileNameBuffer[100];
+	auto index = 0;
+	while (true)
+	{
+		auto nextFrame = videoSpliter->getNextFrame();
+
+		if (nextFrame.empty() && videoSpliter->SplitStatus() == -1)
+			return -2;   // Cannot Open Video File
+
+		if (nextFrame.empty() && videoSpliter->SplitStatus() == -2)
+			return 0;    // To The End
+
+		sprintf_s(outputFileNameBuffer, "%d.png", index);
+		imwrite(outputFileNameBuffer, nextFrame);
+		std::cout << index << "...";
+		++index;
+	}
+}
--- SplitVideoToImageList/main.cpp
+++ SplitVideoToImageList/main.cpp
@@ -1,35 +1,26 @@
 #include <opencv2/core/core.hpp>
 #include "VideoSplitBase.hpp"
 #include <iostream>
+#include "FrameWriterBase.hpp"
 
 using namespace  cv;
 using namespace std;
 
 int main()
 {
-	string videoFileName = "fog_v.avi";
+//	string videoFileName = "fog_v.avi";
+	string videoFileName = "tree.avi";
 	VideoSplitBase videoSpliter;
 	videoSpliter.SetVideoSource(videoFileName);
-	while (true)
-	{
-		auto NextFrame = videoSpliter.getNextFrame();
-		if(NextFrame.empty() && videoSpliter.SplitStatus() == -1)
-		{
-			cout << "Open Video File Failed, Please Check File Existed or Not!" << endl;
-			break;
-		}
 
-		if(NextFrame.empty() && videoSpliter.SplitStatus() == -2)
-		{
-			cout << "To The End of This Video!" << endl;
-			break;
-		}
+	FrameWriterbase frameWriter;
+	frameWriter.SetVideoSpliter(&videoSpliter);
+	auto outputFrameStatus = frameWriter.OutputFrameStream();
 
-		imshow("Next Frame", NextFrame);
-		waitKey(100);
-	}
-
-	destroyAllWindows();
+	if(outputFrameStatus == -2)
+		cout << "Open Video File Failed, Please Check File Existed or Not!" << endl;
+	else if(outputFrameStatus == 0)
+		cout << "To The End of This Video! All Done!" << endl;
 
 	system("pause ");
 
